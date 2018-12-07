b20861eee8fc8fb86d65f916fa84fe40a5b3e95c
novandev <donovan.adams@students.makeschool.com>
2018-12-06 17:42:16 -0800

    added more

---
+++ integration-testing-minitest/test/integration/post_flow_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class PostFlowTest < Capybara::Rails::TestCase
+  def setup
+    @one = posts :one
+    @two = posts :two
+  end
+
+  test 'post index' do
+    visit posts_path
+
+    assert page.has_content?(@one.title)
+    assert page.has_content?(@two.title)
+  end
+  test 'writing a new post' do
+    visit posts_path
+
+    click_on 'New Post'
+
+    fill_in 'Title', with: 'Test Title'
+    fill_in 'Body',  with: 'Test Body'
+
+    click_on 'Create Post'
+
+    assert_current_path post_path(Post.last)
+    assert page.has_content?('Test Title')
+    assert page.has_content?('Test Body')
+  end
+end
