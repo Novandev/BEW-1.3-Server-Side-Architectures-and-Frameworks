75eac8f06c9945cba3192e1af7799912d91b1461
novandev <donovan.adams@students.makeschool.com>
2018-12-12 15:45:53 -0800

    almost done with this priject

--- 13-Final-Project/diary/app/models/contact.rb
+++ 13-Final-Project/diary/app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  # # model association
+  # has_many :items, dependent: :destroy
+  belongs_to :user
+  # validations
+  validates_presence_of :name, :number,:user_id
 end
